7036e5babe7441c1d7648b080e9f95777abd2534
stanleyctg03 <stanley.ching@softwire.com>
2026-09-08 16:05:22 +0100

    Solution for FizzBuzz part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,27 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
-
     // Put your code here...
+    let number = 0;
+    while (number <= 100){
+        if (_isDivisibleByThree(number) && _isDivisibleByFive(number)) {
+            console.log("FizzBuzz");
+        } else if (_isDivisibleByThree(number)) {
+            console.log("Fizz");
+        } else if (_isDivisibleByFive(number)) {
+            console.log("Buzz");
+        } else {
+            console.log(number)
+        }
+        number++
+    }
+}
 
+function _isDivisibleByThree(number) {
+    return number % 3 == 0 ? true : false;
+}
+
+function _isDivisibleByFive(number) {
+    return number % 5 == 0 ? true : false;
 }
 
 // Now, we run the main function:
